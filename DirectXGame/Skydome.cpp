af7663285174aef25aaf5d1d4e61f35354818d39
GC1A_04_ゴ_ウ <147117585+Duohereshui1994@users.noreply.github.com>
2024-04-25 13:57:23 +0900

    02_03_確認課題

--- DirectXGame/Skydome.cpp
+++ DirectXGame/Skydome.cpp
@@ -5,8 +5,9 @@ void Skydome::Initialize(Model* model, ViewProjection* viewProjection) {
 	_model = model;
 	_worldTransform.Initialize();
 	_viewProjection = viewProjection;
+	_worldTransform.scale_ = {150.0f, 150.0f, 150.0f};
 }
 
-void Skydome::Update() { _worldTransform.TransferMatrix(); }
+void Skydome::Update() { _worldTransform.UpdateMatrix(); }
 
 void Skydome::Draw() { _model->Draw(_worldTransform, *_viewProjection); }
